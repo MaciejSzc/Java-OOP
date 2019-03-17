c0f02390c7892621b3478de7cf9991ea947fc44f
Maciej <maciej.szczesny.orzesze@gmail.com>
2019-03-17 22:17:25 +0100

    bike update

--- src/bikes/RentalOffice.java
+++ src/bikes/RentalOffice.java
@@ -1,35 +1,44 @@
 package bikes;
 
 public class RentalOffice {
-    private double income = 0;
+    private double income;
 
     public double getIncome() {
         return income;
     }
 
+
+
     public Bike orderBike(Color color, double tireWidth, boolean aluminium, int days){
         Bike bike = new Bike(color,tireWidth,aluminium);
-          double a = days;
+
           double b;
           double c;
-          double d = tireWidth;
 
-        if(color.equals("red")){
-            c = 500;
-        }else if(color.equals("blue")){
-            c = 250;
-        }else{
-            c= 100;
+        switch (color){
+            case RED:
+                c = 500;
+                break;
+            case BLUE:
+                c = 250;
+                break;
+                default:
+                    c = 100;
         }
+
+
+
         if(aluminium){
-            b = 1;
-        }else{
             b = 2;
+        }else{
+            b = 1;
         }
 
-        bike.setPrice(a*((b*(c + d/3))/20));
-              income += a*((b*(c + d/3))/20);
+
+        income += days*((b*(c + tireWidth/3))/20);
 
         return bike;
     }
 }
+
+
